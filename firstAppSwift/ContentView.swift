8e5fd16e957081840006f98807850926390ca079
Esteban Higuita <esteban.higuita@ceiba.com.co>
2021-08-11 15:26:33 -0500

    Agregar un issue al código

--- firstAppSwift/ContentView.swift
+++ firstAppSwift/ContentView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button("Hello, SwiftUI!") {
+                let seq = 1...4
+                seq.map { $0 }
+            }
+        }
     }
 }
 
