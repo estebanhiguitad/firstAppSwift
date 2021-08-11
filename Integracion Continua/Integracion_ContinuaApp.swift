a70b7cdc901e3ade98f62fe4b58d24eed94941e5
Esteban Higuita <esteban.higuita@ceiba.com.co>
2021-08-11 10:42:03 -0500

    Initial Commit

---
+++ Integracion Continua/Integracion_ContinuaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Integracion_ContinuaApp.swift
+//  Integracion Continua
+//
+//  Created by Esteban Higuita on 11/08/21.
+//
+
+import SwiftUI
+
+@main
+struct Integracion_ContinuaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
